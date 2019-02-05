923fc61c67ba4965d6951d7d7d0b05521edddedc
allbinolsson <al1100ol-s@student.lu.se>
2019-02-05 15:29:44 +0100

    sql file to enter movie data

---
+++ movies_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO movies (imdb_key, title, production_year, playtime)
+VALUES (tt0111161, "The Shawshank Redemption", 1994, 142),
+	(tt0368226, "The Room", 2003, 99),
+	(tt0468569, "The Dark Knight", 2008, 152),
+	(tt0109830, "Forrest Gump", 1994, 142),
+	(tt0110912, "Pulp Fiction", 1994, 154),
+	(tt0108052, "Schindlers List", 1993, 195),
+	(tt0050083, "12 Angry Men", 1957, 96),
+	(tt1375666, "Inception", 2010, 148),
+	(tt0137523, "Fight Club", 1999, 139);
