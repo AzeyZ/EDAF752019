c87313a2859c285191a34b8ded86a6698e93a804
allbinolsson <al1100ol-s@student.lu.se>
2019-02-05 15:52:17 +0100

    customers added to data, tried to remove movies_data

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 INSERT INTO movies (imdb_key, title, production_year, playtime)
-VALUES (tt0111161, "The Shawshank Redemption", 1994, 142),
+VALUES 	(tt0111161, "The Shawshank Redemption", 1994, 142),
 	(tt0368226, "The Room", 2003, 99),
 	(tt0468569, "The Dark Knight", 2008, 152),
 	(tt0109830, "Forrest Gump", 1994, 142),
@@ -8,3 +8,10 @@ VALUES (tt0111161, "The Shawshank Redemption", 1994, 142),
 	(tt0050083, "12 Angry Men", 1957, 96),
 	(tt1375666, "Inception", 2010, 148),
 	(tt0137523, "Fight Club", 1999, 139);
+
+INSERT INTO customers (user_name, full_name, password)
+VALUES 	("gosta", "Gösta Persson", "gösta123"),
+	("gunnhild", "Gunnhild Svensson", "gunnhild123"),
+	("goran", "Göran Olsson", "goran123"),
+	("gurra", "Gustav Olofsson", "gurra123"),
+	("lisa", "Lisa Mattsson", "lisa123");
