44280592135550e280438d83f75e6856b50b3a3b
allbinolsson <al1100ol-s@student.lu.se>
2019-02-06 13:05:20 +0100

    Added the data script to lab.sql

--- lab2.sql
+++ lab2.sql
@@ -26,3 +26,29 @@ CREATE TABLE theaters(
 theather_name TEXT PRIMARY KEY,
 capacity INT
 );
+
+
+
+
+INSERT INTO movies (imdb_key, title, production_year, playtime)
+VALUES 	(tt0111161, "The Shawshank Redemption", 1994, 142),
+	(tt0368226, "The Room", 2003, 99),
+	(tt0468569, "The Dark Knight", 2008, 152),
+	(tt0109830, "Forrest Gump", 1994, 142),
+	(tt0110912, "Pulp Fiction", 1994, 154),
+	(tt0108052, "Schindlers List", 1993, 195),
+	(tt0050083, "12 Angry Men", 1957, 96),
+	(tt1375666, "Inception", 2010, 148),
+	(tt0137523, "Fight Club", 1999, 139);
+
+INSERT INTO customers (user_name, full_name, password)
+VALUES 	("gosta", "Gösta Persson", "gösta123"),
+	("gunnhild", "Gunnhild Svensson", "gunnhild123"),
+	("goran", "Göran Olsson", "goran123"),
+	("gurra", "Gustav Olofsson", "gurra123"),
+	("lisa", "Lisa Mattsson", "lisa123");
+
+INSERT INTO theaters (theater_name, capacity)
+VALUES	("Filmstaden", 127),
+	("Kino", 52),
+	("Folkets Bio i Lund Södran", 50);
