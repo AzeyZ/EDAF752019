1680bd832732970d54a5fe3846c61d46462a86ff
allbinolsson <al1100ol-s@student.lu.se>
2019-02-07 11:33:04 +0100

    Started trying in notebook

--- lab2.sql
+++ lab2.sql
@@ -8,43 +8,43 @@ DROP TABLE IF EXISTS tickets;
 CREATE TABLE tickets(
 id TEXT PRIMARY KEY,
 theater_name TEXT REFERENCES theather(theather_name),
-movie_name TEXT REFERENCES movies(title),
+imdb_key TEXT REFERENCES movies(imdb_key),
 user_name TEXT REFERENCES customers(user_name),
 ticket_date DATE,
 ticket_time TIME
 );
 
-DROP TABLE IF EXISTS screenings
-CREATE TABLE performances (
+DROP TABLE IF EXISTS screenings;
+CREATE TABLE screenings(
 start_time TIME,
 screening_date DATE,
 movie_name TEXT REFERENCES movies(title),
-PRIMARY KEY (start_time, screening_date);
+PRIMARY KEY (start_time, screening_date)
 );
 
 DROP TABLE IF EXISTS movies;
 CREATE TABLE movies(
-imdb_key INTEGER PRIMARY KEY,
+imdb_key TEXT PRIMARY KEY,
 title TEXT,
 production_year INT,
 playtime INT);
 
 DROP TABLE IF EXISTS theaters;
 CREATE TABLE theaters(
-theather_name TEXT PRIMARY KEY,
+theater_name TEXT PRIMARY KEY,
 capacity INT
 );
 
 INSERT INTO movies (imdb_key, title, production_year, playtime)
-VALUES 	(tt0111161, "The Shawshank Redemption", 1994, 142),
-	(tt0368226, "The Room", 2003, 99),
-	(tt0468569, "The Dark Knight", 2008, 152),
-	(tt0109830, "Forrest Gump", 1994, 142),
-	(tt0110912, "Pulp Fiction", 1994, 154),
-	(tt0108052, "Schindlers List", 1993, 195),
-	(tt0050083, "12 Angry Men", 1957, 96),
-	(tt1375666, "Inception", 2010, 148),
-	(tt0137523, "Fight Club", 1999, 139);
+VALUES 	("tt0111161", "The Shawshank Redemption", 1994, 142),
+	("tt0368226", "The Room", 2003, 99),
+	("tt0468569", "The Dark Knight", 2008, 152),
+	("tt0109830", "Forrest Gump", 1994, 142),
+	("tt0110912", "Pulp Fiction", 1994, 154),
+	("tt0108052", "Schindlers List", 1993, 195),
+	("tt0050083", "12 Angry Men", 1957, 96),
+	("tt1375666", "Inception", 2010, 148),
+	("tt0137523", "Fight Club", 1999, 139);
 
 INSERT INTO customers (user_name, full_name, pass_word)
 VALUES 	("gosta", "Gösta Persson", "gösta123"),
@@ -57,3 +57,6 @@ INSERT INTO theaters (theater_name, capacity)
 VALUES	("Filmstaden", 127),
 	("Kino", 52),
 	("Folkets Bio i Lund Södran", 50);
+
+INSERT INTO tickets (imdb_key, ticket_date)
+VALUES	("tt0368226", '2019-02-01');
