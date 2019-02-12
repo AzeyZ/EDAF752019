315cf948e5f3c0f0f1cf87a0bad456e7f77bfaf4
Andreas Erlandsson <aner96@gmail.com>
2019-02-12 12:46:17 +0100

    adding code for app

---
+++ java/lab3_edaf75/src/lab3_edaf75/App.java
@@ -0,0 +1,24 @@
+package lab3_edaf75;
+
+public class App {
+
+    public static void main(String[] args) {
+        new App().run();
+    }
+
+    Database db = new Database();
+
+    void run() {
+        db.openConnection("db.sqlite");
+        for (var student : db.getStudentInfo("Stanford", "CS")) {
+            System.out.println(student.name + ": " + student.gpa);
+        }
+
+        System.out.println("Most popular colleges/majors");
+        for (var ai : db.getApplicationInfo()) {
+            System.out.println(ai.count + ":" + ai.college + "/" + ai.major);
+        }
+
+        db.gradeFix("Stanford", 0.04);
+    }
+}
