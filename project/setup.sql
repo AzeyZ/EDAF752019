809f2c58ceac6111e5c7bbd608d631845c2ff81b
allbinolsson <al1100ol-s@student.lu.se>
2019-03-12 10:35:02 +0100

    continued with setup

--- project/setup.sql
+++ project/setup.sql
@@ -23,35 +23,51 @@ amount INT
 );
 
 CREATE TABLE used_materials (
-used_amount INT
+used_amount INT,
+-- Unsure about the keys, feel free to change
+FOREIGN KEY (ingredient) REFERENCES materials (ingredient),
+PRIMARY KEY (used_amount, ingredient)
 );
 
 CREATE TABLE restocks (
 buy_amount INT,
 buy_date DATE,
-buy_time TIME
+buy_time TIME,
+-- Unsure about the keys, feel free to change
+FOREIGN KEY (ingredient) REFERENCES materials (ingredient),
+PRIMARY KEY (ingredient, buy_amount, buy_date, buy_time)
 );
 
 CREATE TABLE pallets (
-pallet_id TEXT DEFAULT (lower(hex(randomblop()))),
+pallet_id TEXT DEFAULT (lower(hex(randomblob()))),
 production_date DATE,
 produection_time TIME,
-block INT
+block INT,
+PRIMARY KEY (pallet_id)
 );
 
 CREATE TABLE deliveries (
 delivery_date DATE,
-delivery_time TIME
+delivery_time TIME,
+-- Unsure about the keys, feel free to change
+-- Might want to add delivery id?
+FOREIGN KEY (pallet_id) REFERENCES pallets (pallet_id),
+FOREIGN KEY (customer_name) REFERENCES customers (name),
+PRIMARY KEY (delivery_date, delivery_time, pallet_id, customer_name)
 );
 
 CREATE TABLE customers (
-name TEXT,
+name TEXT PRIMARY KEY,
 address TEXT
 );
 
 CREATE TABLE orders (
 due_date DATE,
-due_time TIME
+due_time TIME,
+-- Unsure about the keys, feel free to change
+FOREIGN KEY (product_name) REFERENCES products (product_name),
+FOREIGN KEY (customer_name) REFERENCES customers (name),
+PRIMARY KEY (due_date, due_time, product_name, customer_name)
 );
 
 
